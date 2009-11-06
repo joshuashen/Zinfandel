f0d67a5be3c64c1c1f928a39ed3a6d9c8ed62e65
unknown <yiweigu@.(none)>
2009-11-06 08:08:35 -0500

    Bimodal Distribution

--- Java/EmissionDistanceMatrix.java
+++ Java/EmissionDistanceMatrix.java
@@ -20,7 +20,8 @@ emissionDistanceMatrixNegStrand = new double[numStates][maxDistance+1];
 for (int state = 0; state < numStates; state++ ) {
 // initial grid state: positive strand distance is larger; negative strand avg
 if (states.get(state) instanceof FiveFlankingState) {
-emissionDistanceMatrixPosStrand[state][dist] = logNormal(dist, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
+emissionDistanceMatrixPosStrand[state][dist] = logBimodal(dist, avgDistance, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
+//emissionDistanceMatrixPosStrand[state][dist] = logNormal(dist, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
 emissionDistanceMatrixNegStrand[state][dist] = logNormal(dist, avgDistance, standardDevDistance);
 
 }
@@ -28,7 +29,8 @@ emissionDistanceMatrixNegStrand[state][dist] = logNormal(dist, avgDistance, stan
 // final grid state: negative strand distance is larger; positive strand avg
 else if (states.get(state) instanceof ThreeFlankingState) {
 emissionDistanceMatrixPosStrand[state][dist] = logNormal(dist, avgDistance, standardDevDistance);
-emissionDistanceMatrixNegStrand[state][dist] = logNormal(dist, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
+//emissionDistanceMatrixNegStrand[state][dist] = logNormal(dist, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
+emissionDistanceMatrixNegStrand[state][dist] = logBimodal(dist, avgDistance, ((GridState)states.get(state)).delSize + avgDistance, standardDevDistance);
 }
 else {
 emissionDistanceMatrixPosStrand[state][dist] = logNormal(dist, avgDistance, standardDevDistance);
@@ -51,7 +53,21 @@ emissionDistanceMatrixNegStrand[state][dist] = logNormal(dist, avgDistance, stan
 //}
         System.out.println("Emission Matrix Created");
     }
-
+    
+    public double logBimodal(int dis, double avg1, double avg2, double dev){        
+        double p1 = (-1) * ((double)dis - avg1) * ((double)dis - avg1);
+        double p2 = (-1) * ((double)dis - avg2) * ((double)dis - avg2);
+        p1 = p1/(2 * dev * dev);
+        p2 = p2/(2 * dev * dev);
+        p1 = Math.exp(1);
+        p2 = Math.exp(p2);
+        double x = Math.log(p1 + p2) - Math.log(2 * dev * Math.sqrt(2*Math.PI));
+        if (x < this.minEmissionfromDistance) {
+         x = this.minEmissionfromDistance;
+        }
+        return x;        
+    }
+    
     //log(Normal, k, average, std deviation) = exp^(-(x-avg)^2) / 2*dev^2 - log(dev * sqrt(2pi))
     public double logNormal(int dis, double avg, double dev){
         double p = (-1) * ((double)dis - avg) * ((double)dis - avg);
